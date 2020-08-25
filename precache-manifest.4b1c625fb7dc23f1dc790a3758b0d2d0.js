ba403af4de45315db5313e782fd2990e5584314b
andreviich <31255901+andreviich@users.noreply.github.com>
2020-08-25 13:27:52 +0300

    Add files via upload

---
+++ precache-manifest.4b1c625fb7dc23f1dc790a3758b0d2d0.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "06a5250151093e748d90dacde48c8836",
+    "url": "/index.html"
+  },
+  {
+    "revision": "7b0dd1582a0ae425c19b",
+    "url": "/static/css/main.11f0bf91.chunk.css"
+  },
+  {
+    "revision": "ef23a7c36c89ecf88603",
+    "url": "/static/js/2.d3c03343.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/static/js/2.d3c03343.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "7b0dd1582a0ae425c19b",
+    "url": "/static/js/main.7ec29a30.chunk.js"
+  },
+  {
+    "revision": "e882e74e7ec39debd5c7",
+    "url": "/static/js/runtime-main.c2945744.js"
+  }
+]);
